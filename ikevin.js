7c4e47feb4afdd526bc7adfd45664bb2cec660b8
Kevin <kevin@Kevins-MacBook-Air.local>
2025-03-26 12:10:07 +0200

    endswith work explanation

---
+++ ikevin.js
@@ -0,0 +1,9 @@
+const str1 = "Cats are the best!";
+
+console.log(str1.endsWith("best!"));
+
+console.log(str1.endsWith("best", 17));
+
+const str2 = "Is this a question?";
+
+console.log(str2.endsWith("question"));
