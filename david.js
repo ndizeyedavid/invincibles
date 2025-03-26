aa7238f995487cd8d2b3390065a95b482c8a1233
ndizeyedavid <davidndizeye101@gmail.com>
2025-03-26 11:46:28 +0200

    david's file

--- david.js
+++ david.js
@@ -8,4 +8,5 @@ return a character assigned to the index you provided
 
 const mi_name = "Mellow";
 console.log(mi_name.at(0)) // output: M
+console.log(mi_name.at(3)) // output: l
 console.log(mi_name.at(-1)) // output: w
