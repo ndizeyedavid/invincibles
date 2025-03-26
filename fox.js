4765ad49564803d1994b986fd3c073488c86e215
ndizeyedavid <davidndizeye101@gmail.com>
2025-03-26 12:09:36 +0200

    explain work team

---
+++ fox.js
@@ -0,0 +1,10 @@
+// String.prototype.concat() is a method used to concatenate two or more strings.
+// It is a part of the String object and allows you to join multiple strings together into a single string.
+
+let str1 = "Hello";
+let str2 = "World";
+let result = str1.concat(" ", str2, "!");
+console.log(result); 
+
+// Output: "Hello World!"
+
